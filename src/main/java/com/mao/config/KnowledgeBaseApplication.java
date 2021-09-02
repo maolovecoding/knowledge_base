49ba76a63fc1018aeeb277a79f92ba00b1d471a9
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2021-09-02 16:13:36 +0800

    更新了启动类的配置，测试了http请求，继承springboot的热部署

--- src/main/java/com/mao/config/KnowledgeBaseApplication.java
+++ src/main/java/com/mao/config/KnowledgeBaseApplication.java
@@ -1,14 +1,23 @@
-package com.mao.knowledge_base;
+package com.mao.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+/**
+ * @author 毛毛
+ * @SpringBootApplication: springboot的启动类配置注解
+ * @ComponentScan: 配置扫描的包
+ */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mao.controller", "com.mao.service"})
 public class KnowledgeBaseApplication {
-    //日志对象
+    /**
+     * 日志对象
+     */
     public static final Logger LOG = LoggerFactory.getLogger(KnowledgeBaseApplication.class);
 
     public static void main(String[] args) {
@@ -19,5 +28,4 @@ public class KnowledgeBaseApplication {
         //地址 端口号 通过环境配置对象可以获取到端口号
         LOG.info("地址： \t http://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
